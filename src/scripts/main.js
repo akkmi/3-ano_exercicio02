c744fa065aeb2bac0229b7cd925b8cafcd5114ae
akkmi <akkemjjraa@gmail.com>
2025-02-24 12:28:52 -0300

    24/02/2025 12:28

---
+++ src/scripts/main.js
@@ -0,0 +1,11 @@
+function sortear() {
+    // pegar o total de resultados 
+    document_querySelector('')
+    // pegar o menor valor 
+
+    // pegar o maior valor
+
+    // gerar um numero aleatorio entre o menor e maior valor
+
+    // exibir o resultado em html
+}
